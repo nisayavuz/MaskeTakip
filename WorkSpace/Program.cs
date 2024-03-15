91fdcbc6158703d46ec14e9f85b4b2734283aa0b
Nisa Yavuz <nsnryvz@outlook.com>
2024-03-15 23:38:32 +0300

    Add project files.

---
+++ WorkSpace/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace WorkSpace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SelamVer();
+
+            Console.ReadLine();
+        }
+
+        static void SelamVer()
+        {
+            Console.WriteLine("Merhaba");
+        }
+    }
+
+}
